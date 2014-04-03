ec74ec22bc62779ac48d261d3d430efaf854373a
PatoBeltran <pbelsal@gmail.com>
2014-04-03 15:15:51 -0600

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mexicourbano::Application.config.secret_key_base = '5f60e0f1fdd907e817a8907b850609cbe4fca0e2983a6dcb6c1d2e74040309e59cbb32ac60547d4abbcef6a02a994cda3774ec49d5285bb2eeed6f4a061aeb41'
