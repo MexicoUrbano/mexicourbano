6d4144eb7798d6e39b7ff546291a42b235dcc289
luis loya <jl.loya@gmail.com>
2014-04-27 19:27:39 -0500

    Added a little seed to bootstrap the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admins = [
+            {:email=>"admin@admin.com", :password=>"mexicourbano"}
+]
+          
+advisors = [
+            {:email=>"advisor1@advisor1.com", :password=>"mexicourbano"},
+            {:email=>"advisor2@advisor2.com", :password=>"mexicourbano"}
+]
+          
+troopers = [
+            {:email=>"trooper1@trooper2.com", :password=>"mexicourbano"},
+            {:email=>"trooper1@trooper.com", :password=>"mexicourbano"}
+]
+
+admins.each do |attributes|
+  Admin.create do |a|
+    a.email = attributes[:email]
+    a.password = attributes[:password]
+  end
+end
+
+advisors.each do |attributes|
+  Advisor.create do |a|
+    a.email = attributes[:email]
+    a.password = attributes[:password]
+  end
+end   
+
+troopers.each do |attributes|
+  Trooper.create do |t|
+    t.email = attributes[:email]
+    t.password = attributes[:password]
+  end
+end 
