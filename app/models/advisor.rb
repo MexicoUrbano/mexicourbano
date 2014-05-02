2f18c15009281f4069378b1d6eee7ca5adc4f6bf
luis loya <jl.loya@gmail.com>
2014-05-02 17:55:07 -0500

    Finished Advisors Show Template

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -11,4 +11,11 @@ class Advisor < ActiveRecord::Base
   acts_as_user
   has_many :troopers
 
+  # Este metodo deja unicamente los numeros contenidos
+  # en el string de phone
+  # 664-661-12-50 => 66416611250
+  # +664 66-12-50 => 6616611250
+  def telefono
+    phone.gsub /[^\d]/, ''
+  end
 end
