d2ff6a3668b12385a130139b212ce4fddd902eaf
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-03 21:50:32 -0500

    Validates Advisor :phone

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -10,6 +10,9 @@
 class Advisor < ActiveRecord::Base
   acts_as_user
   has_many :troopers
+  validates :phone, length: { minimum: 10}
+  validates :phone, numericality: true
+  before_validation :telefono
   attr_accessor :password_temp
 
   # Este metodo deja unicamente los numeros contenidos
@@ -17,7 +20,7 @@ class Advisor < ActiveRecord::Base
   # 664-661-12-50 => 66416611250
   # +664 66-12-50 => 6616611250
   def telefono
-    phone.gsub /[^\d]/, ''
+    self.phone = phone.gsub /[^\d]/, ''
   end
 
   def password_temp=(param)
