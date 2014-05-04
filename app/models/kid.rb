07fbc59bcfac46f99d2b4665899f9228f97f2c56
luis loya <jl.loya@gmail.com>
2014-05-04 01:00:12 -0500

    Added grade field to Kids model

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -12,8 +12,20 @@
 #
 
 class Kid < ActiveRecord::Base
+  GRADOS = ['Primero',
+            'Segundo',
+            'Tercero',
+            'Cuarto',
+            'Quinto',
+            'Sexto']
+            
   mount_uploader :image, ImageUploader
   belongs_to :trooper
+
+  def grade_name
+    GRADOS[grade-1] # 3 => Tercero  
+  end
+  
   def age
     now = Time.now.utc.to_date
     now.year - self.birthdate.year - ((now.month > self.birthdate.month || (now.month == self.birthdate.month && now.day >= self.birthdate.day)) ? 0 : 1)
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,13 @@ troopers = [
             {:email=>"trooper2@trooper2.com", :password=>"mexicourbano", :advisor_id=>1, :name=>"Israel Perez"}
 ]
 
+kids = [
+            {:name=>"Eva Rangel Araujo",      :birthdate=>"23/07/2005", :grade=>4, :trooper_id=>1},
+            {:name=>"Carlos Lopez Lopez",     :birthdate=>"18/08/2006", :grade=>3, :trooper_id=>1},
+            {:name=>"Carolina Gomez Urrutia", :birthdate=>"10/06/2008", :grade=>1, :trooper_id=>2},
+            {:name=>"Roberto Garza Sada",     :birthdate=>"4/09/2009",  :grade=>1, :trooper_id=>2}
+]
+
 admins.each do |attributes|
   Admin.create do |a|
     a.email = attributes[:email]
@@ -45,3 +52,12 @@ troopers.each do |attributes|
     t.name = attributes[:name]
   end
 end 
+
+kids.each do |attributes|
+  Kid.create do |k|
+    k.name = attributes[:name]
+    k.birthdate = attributes[:birthdate]
+    k.grade = attributes[:grade]
+    k.trooper_id = attributes[:trooper_id]
+  end
+end
