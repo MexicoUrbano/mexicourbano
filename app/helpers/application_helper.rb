e025b1bbb4b4a2af27ff45f29ff5bb95e5858f8e
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-03 21:04:11 -0500

    User show and edit itself from header links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
-  
+
 	def is_active?(page_name)
     "class=\"active\"" if request.path_info.include?(page_name)
   end
 
-  
+  def userable_path
+     eval "#{current_user.userable_type.downcase}_path(current_user.userable_id)"
+  end
+
+	def edit_userable_path
+		eval "edit_#{current_user.userable_type.downcase}_path(current_user.userable_id)"
+	end
+
 end
