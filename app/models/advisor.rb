e8e7b35365ec74a3cf310df92cff9291a374c6ed
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-03 21:57:56 -0500

    Advisor can select a community.

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -8,6 +8,20 @@
 #
 
 class Advisor < ActiveRecord::Base
+  COMUNIDADES = ["La Cáscara, Montemorelos",
+                  "Loma Alta, Linares",
+                  "El Galeme, Montemorelos",
+                  "El Roble, Montemorelos",
+                  "Ejido Chihuaha, Montemorelos",
+                  "Lirios, Montemorelos",
+                  "Jauregui, Allende",
+                  "Paso Hondo, Allende",
+                  "Lazarillos de Abajo, Allende",
+                  "San Antonio, Allende",
+                  "El Terrero, Montemorelos",
+                  "San José, Linares"
+                ]
+                
   acts_as_user
   has_many :troopers
   validates :phone, length: { minimum: 10}
@@ -19,6 +33,11 @@ class Advisor < ActiveRecord::Base
   # en el string de phone
   # 664-661-12-50 => 66416611250
   # +664 66-12-50 => 6616611250
+  
+  def self.communities
+    COMUNIDADES
+  end
+  
   def telefono
     self.phone = phone.gsub /[^\d]/, ''
   end
